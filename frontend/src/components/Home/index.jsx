cfd3f6badd00b28d4351c5ec19095321e4a96ca7
IbrahimHassoun1 <ihassoun73@gmail.com>
2025-04-03 02:10:09 +0300

    connected the analytics api

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
-import './styles.css'
+// App.js
+import React, { useState } from "react";
+import "./styles.css";
+import request from "../../utils/remote/axios";
+import requestMethods from "../../utils/enums/request.methods";
+
+const App = () => {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [totalSteps,setTotalSteps] = useState(0);
+  const [totalDistance,setTotalDistance] = useState(0);
+  const [totalMinutes,setTotalMinutes] = useState(0);
+  const [loading,setLoading] = useState(false);
 
 
-const Home = () => {
+  const handleFilter = async() => {
+   try {
+    setLoading(true)
+    console.log(startDate)
+    console.log(endDate)
+    const response = await request({
+        method:requestMethods.POST,
+        route:"api/v0.1/data",
+        body:{
+             "start_date":startDate,
+            "end_date":endDate
+        }
+    })
+    console.log(response)
+    if(response.success){
+        setTotalDistance(response.data.total_distance)
+        setTotalMinutes(response.data.total_minutes)
+        setTotalSteps(response.data.total_steps)
+    }
+    setLoading(false)
+   } catch (error) {
+    console.log(error)
+   }
+   
+  };
+
   return (
-    <div>index</div>
-  )
-}
+    <div className="app">
+      <div className="filter">
+        <input
+          type="date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        />
+        <input
+          type="date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+        />
+        <button onClick={handleFilter}>Filter</button>
+      </div>
+      {loading?<p>Please Wait...</p>:""}
+      <div className="stats-container">
+        <div className="stat-box">
+          <h3>Total Steps</h3>
+          <p>{totalSteps}</p>
+        </div>
+        <div className="stat-box">
+          <h3>Total Distance (km)</h3>
+          <p>{totalDistance}</p>
+        </div>
+        <div className="stat-box">
+          <h3>Total Minutes</h3>
+          <p>{totalMinutes}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default App;
